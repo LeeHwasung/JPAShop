293bd97e129417f4d6dbb36cd386c1ba0f20b5b2
LeeHwaSung <hsdayfly@gmail.com>
2021-02-04 01:09:34 +0900

    Project Create

---
+++ src/main/java/jpabook/jpashop/Hello.java
@@ -0,0 +1,12 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class Hello {
+	private String data;
+}
